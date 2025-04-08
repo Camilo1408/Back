e6de6fbab7d61d033238026c54ccf43373a7c1fd
Camilo1408 <121272695+Camilo1408@users.noreply.github.com>
2025-04-08 15:59:03 -0500

    Back Full

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { createAccessToken } from "../libs/jwt.js";
 
 export const register = async (req, res) => {
   const { email, password, username } = req.body;
@@ -14,28 +15,70 @@ export const register = async (req, res) => {
       password: passwordHash,
     });
     const userSaved = await newUser.save();
-    jwt.sign(
-      { id: userSaved._id },
-      "secret123",
-      {
-        expiresIn: "1d",
-      },
-      (err, token) => {
-        if (err) console.log(err);
-        res.json({ token });
-      }
-    );
+    const token = await createAccessToken({ id: userSaved._id });
+
+    res.cookie("token", token);
+
     // console.log(newUser);
-    // res.json({
-    //   id: userSaved._id,
-    //   username: userSaved.username,
-    //   email: userSaved.email,
-    //   createAt: userSaved.createdAt,
-    //   updateAt: userSaved.updatedAt,
-    // });
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createAt: userSaved.createdAt,
+      updateAt: userSaved.updatedAt,
+    });
   } catch (error) {
-    console.log(error);
+    res.status(500).json({ message: error.message });
   }
 };
 
-export const login = (req, res) => res.send("login");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const userFound = await User.findOne({ email });
+    if (!userFound)
+      return res.status(400).json({ message: "Usuario no encontrado" });
+
+    const isMatch = await bcrypt.compare(password, userFound.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Password incorrecta" });
+
+    const token = await createAccessToken({ id: userFound._id });
+
+    res.cookie("token", token);
+
+    // console.log(newUser);
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      createAt: userFound.createdAt,
+      updateAt: userFound.updatedAt,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const logout = async (req, res) => {
+  res.cookie("token", "", {
+    expires: new Date(0),
+  });
+  return res.sendStatus(200);
+};
+
+export const profile = async (req, res) => {
+  const userFound = await User.findById(req.user.id);
+
+  if (!userFound)
+    return res.status(400).json({ message: "Usuario no encontrado" });
+
+  return res.json({
+    id: userFound._id,
+    username: userFound.username,
+    email: userFound.email,
+    createAt: userFound.createdAt,
+    updateAt: userFound.updatedAt,
+  });
+};
